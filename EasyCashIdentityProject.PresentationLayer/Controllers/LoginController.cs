947eb432add99dbea64cb939ad2e1973dc4fada6
Umeyr13 <umeyr.gulcimen@gmail.com>
2023-06-07 20:03:58 +0300

    Ders_19_Sonu

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using EasyCashIdentityProject.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public LoginController(SignInManager<AppUser> signInManager, UserManager<AppUser> usermanager)
+        {
+            _signInManager = signInManager;
+            _userManager = usermanager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel loginViewModel)
+        {
+            var result = await _signInManager.PasswordSignInAsync(loginViewModel.Username,loginViewModel.Password,false/*beni hatırla özelliği cookie de tutulsun mu*/,true/*5 defa dan fazla şifreyi yanlış girerse kullanıcıya 5 dk veya neyse engel koyma*/);
+
+            if (result.Succeeded) 
+            {
+                var user = await _userManager.FindByNameAsync(loginViewModel.Username);
+                if (user.EmailConfirmed == true) 
+                {
+                    return RedirectToAction("Index","MyProfile");
+                
+                }
+
+            }
+
+            return View(); 
+        
+        }
+    }
+}
